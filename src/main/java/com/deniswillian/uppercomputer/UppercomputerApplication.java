ecd6e9fc3096650bde30ca56bf85deda0c6729d4
Denis Liborges <deniswliborges@gmail.com>
2021-01-16 00:14:20 -0300

    Projeto Criado

---
+++ src/main/java/com/deniswillian/uppercomputer/UppercomputerApplication.java
@@ -0,0 +1,13 @@
+package com.deniswillian.uppercomputer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppercomputerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppercomputerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deniswillian/uppercomputer/UppercomputerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deniswillian.uppercomputer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppercomputerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
