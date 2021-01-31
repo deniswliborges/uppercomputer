4679eab1157274f8b968d0c0c4821564dbb478f7
Denis Liborges <deniswliborges@gmail.com>
2021-01-31 19:20:28 -0300

    Configurando Properties para ambiente TEST, Criando Class DBService para os insert no BD e Configurando Class TestConfig para obejto de Service @Bean ser instanciado,montado e gerenciado

--- src/main/java/com/deniswillian/uppercomputer/config/TestConfig.java
+++ src/main/java/com/deniswillian/uppercomputer/config/TestConfig.java
@@ -8,16 +8,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.deniswillian.uppercomputer.services.DBService;
 
 
-//@Configuration
-//@Profile("test")
+
+@Configuration
+@Profile("test")
 public class TestConfig {
 
+	@Autowired
+	public DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+		private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		// objeto da class Services instanciado, montado e gerenciado pelo Spring
+		dbService.instantiateTestDatabase();		
+		return true;
+	}
 	
 	
-	//@Value("${spring.jpa.hibernate.ddl-auto}")
-	//private String strategy;
 
 
 }
