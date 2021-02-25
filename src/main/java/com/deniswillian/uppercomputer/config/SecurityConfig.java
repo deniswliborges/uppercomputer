aa8634a097fa7bb009a2cc7a596fb024388aac49
Denis Liborges <deniswliborges@gmail.com>
2021-02-25 19:13:54 -0300

    Adicionando senha ao Funcionario e preparando para Autenticação

--- src/main/java/com/deniswillian/uppercomputer/config/SecurityConfig.java
+++ src/main/java/com/deniswillian/uppercomputer/config/SecurityConfig.java
@@ -7,11 +7,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -24,9 +26,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	//Liberar acesso no ambiente de test que utiliza Banco H2
 	@Autowired
 	private Environment env;
+	
+	
 
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
@@ -51,6 +58,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,
 		// userDetailsService));											//Stateless não cria seção de usuário
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 
 	@Bean
